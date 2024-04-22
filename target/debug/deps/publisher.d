febe36de81927883bdc890ec09a3e99e3ea8b503
KenishaJazlyn <kenishajazlyn@gmail.com>
2024-04-22 13:01:15 +0700

    Initial publisher code

---
+++ target/debug/deps/publisher.d
@@ -0,0 +1,5 @@
+C:\Users\LENOVO\Documents\kuliah\TERM4\ADPRO\tutorial8\publisher\target\debug\deps\publisher.exe: src\main.rs
+
+C:\Users\LENOVO\Documents\kuliah\TERM4\ADPRO\tutorial8\publisher\target\debug\deps\publisher.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/publisher.d
@@ -0,0 +1,1 @@
+C:\Users\LENOVO\Documents\kuliah\TERM4\ADPRO\tutorial8\publisher\target\debug\publisher.exe: C:\Users\LENOVO\Documents\kuliah\TERM4\ADPRO\tutorial8\publisher\src\main.rs
